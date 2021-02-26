f6203686716ccce6068313b3557143a9c6c5edd0
sonabyanjankar <54607479+sonabyanjankar@users.noreply.github.com>
2021-02-25 21:28:59 -0600

    branch3 added

---
+++ src/HelloWorld/branch3.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class branch3 {
+
+        @Test
+        public void test3(){
+
+            String branch = "Branch3";
+            Assert.assertEquals("Branch3", branch);
+        }
+    }
+
+
